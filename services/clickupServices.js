01760f6780aaaccb4cbcdc51f59f5ab21e3b9cb5
SocialBureauWeb <web@socialbureau.in>
2025-11-07 13:58:43 +0530

    Backend 3

--- services/clickupServices.js
+++ services/clickupServices.js
@@ -12,11 +12,9 @@ export const dashboardAPI=async()=>{
 export const userDetailsAPI=async(name)=>{
     // const userToken=getToken()  
     const response = await axios.get(`${BASE_URL}/clickup/user-details`, {
-  params: { name },
-  headers: { "Accept": "application/json" }
-});
-
-  console.log(name,response);
-  
+    params: { name},
+    headers: { "Accept": "application/json" }
+  });
+    console.log(name,response);  
     return response.data
 }
