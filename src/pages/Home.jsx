35c16ce0a68f767e66f5710fe3d835bb36695e27
SocialBureauWeb <web@socialbureau.in>
2026-05-29 14:01:43 +0530

    cover img rmv

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -468,7 +468,7 @@ export const Home = () => {
       <StudentTicker />
 
       
-      <section className="w-full px-0">
+      {/* <section className="w-full px-0">
         <a href='/' target="_blank" rel="noopener noreferrer">
           <img
             src={currentHero.desktop}
@@ -481,7 +481,7 @@ export const Home = () => {
             className={`block sm:hidden w-full object-contain ${currentHero.mobile ? 'h-full' : 'h-auto'}`}
           />
         </a>
-      </section>
+      </section> */}
       
       {/* <section className="w-full px-0"> <a href='https://www.instagram.com/reel/DXV3lbVCb77' target="_blank" rel="noopener noreferrer">
   <video
