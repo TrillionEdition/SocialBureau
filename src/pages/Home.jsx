adf592a4370a5842425279b470752be5cca578fc
SocialBureauWeb <web@socialbureau.in>
2026-06-17 18:55:05 +0530

    Fifa popup 5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -802,7 +802,7 @@ export const Home = () => {
     "https://pub-dbc24446d37a40aeb1dfdd10992cd2d9.r2.dev/2083f53d-bc28-48a0-86fc-bcac92b54984.png",
   ],
   mobile: [
-    "https://pub-dbc24446d37a40aeb1dfdd10992cd2d9.r2.dev/a3db5f8d-ecaf-4d44-b51b-4239adbccc83.png",
+    "https://pub-dbc24446d37a40aeb1dfdd10992cd2d9.r2.dev/2083f53d-bc28-48a0-86fc-bcac92b54984.png",
     "https://pub-dbc24446d37a40aeb1dfdd10992cd2d9.r2.dev/ChatGPT%20Image%20Jun%2011%2C%202026%2C%2006_27_01%20PM.png",
   ],
   alt: "Social Bureau",
